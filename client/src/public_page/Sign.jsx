17d5a8d6f30f30f9d162b436317ae71b6bbe753f
Ocece77 <oceanekasindupro@gmail.com>
2024-07-09 20:46:21 +0200

    assets

--- client/src/public_page/Sign.jsx
+++ client/src/public_page/Sign.jsx
@@ -32,7 +32,7 @@ const Sign = () => {
     }
     isLoading(true)
     try{
-      const res = await fetch("http://localhost:3000/api/auth/sign",{
+      const res = await fetch("/api/auth/sign",{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
