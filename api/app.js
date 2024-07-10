b2db7be4f33b0f1d16fd6c637db70b0499767d66
Ocece77 <oceanekasindupro@gmail.com>
2024-07-10 13:24:20 +0200

    put the join

--- api/app.js
+++ api/app.js
@@ -27,8 +27,11 @@ const __dirname = path.dirname(__filename)
 app.use(express.static(path.join(__dirname,'../client/dist')))
 app.use(cors())
 app.use(helmet());
-
 app.use(cookieParser());
+
+app.get("*", (req , res)=>{
+  res.sendFile(path.join(__dirname ,'../client/dist/index.html' ))
+})
 
 app.use("/api/task" , taskRoutes )
 app.use("/api/auth" , authRoutes )
